2a1bad416059d308c0094dad874b71bde29c55f4
Hero Ben <34982669+HeroBenHero@users.noreply.github.com>
2022-07-17 10:44:22 +0530

    Create autorclone.sh

---
+++ autorclone.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/HeroTest5/AutoRclone.git
+cd AutoRclone/
+sudo pip install -r requirements.txt
+python rclone_sa_magic.py -s 1Dg-iegKhoWNn45BehHiwAkPdjcfr1XAE -d 0ABXqX5_j5hvMUk9PVA -dp Ben10 -b 1 -e 600
