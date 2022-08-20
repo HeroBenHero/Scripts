9bf4a16d773b578c96fbc37358b95c69823a6562
Hero Ben <34982669+HeroBenHero@users.noreply.github.com>
2022-08-21 00:09:27 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
 curl https://rclone.org/install.sh | sudo bash
 curl 'https://raw.githubusercontent.com/HeroBenHero/HeroBenHero.github.io/main/Kodi/Latest/rclone.conf' > rclone.conf
-rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" Shared2:"Ben 10" --config=rclone.conf --drive-acknowledge-abuse
-rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" OnedriveBusiness1:"Ben 10" --config=rclone.conf --drive-acknowledge-abuse
-rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" OnedriveBusiness:"Ben 10" --config=rclone.conf --drive-acknowledge-abuse
+#rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" Shared2:"Ben 10" --config=rclone.conf --drive-acknowledge-abuse
+#rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" OnedriveBusiness1:"Ben 10" --config=rclone.conf --drive-acknowledge-abuse
+#rclone sync Shared:HeroBenHero/Cartoons/"Ben 10" OnedriveBusiness:"Ben 10" --config=rclone.conf --drive-acknowledge-abuse
+rclone sync Shared:GdriveXbot OnedriveBusiness1:GdriveXbot --config=rclone.conf --drive-acknowledge-abuse
+rclone sync Shared:HeroBenHero/Movies OnedriveBusiness1:Movies --config=rclone.conf --drive-acknowledge-abuse
